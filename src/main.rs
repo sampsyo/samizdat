c2e0a339a115133bc59c987a8ee9ef1bc21488c8
Adrian Sampson <adrian@radbox.org>
2021-07-31 13:47:00 -0700

    A data type enum

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,75 @@
 use structopt::StructOpt;
 use std::io::{self, BufRead};
+use std::str::FromStr;
+use std::string::ToString;
+use std::fmt;
 use fraction::{BigDecimal, ToPrimitive};
+
+#[derive(Debug, Clone, Copy)]
+enum DataType {
+    Float32,
+    Float64,
+    Fixed(usize, usize),
+}
+
+#[derive(Debug, Clone)]
+struct ParseTypeError;
+
+impl FromStr for DataType {
+    type Err = ParseTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s == "f32" {
+            Ok(DataType::Float32)
+        } else if s == "f64" {
+            Ok(DataType::Float64)
+        } else {
+            panic!("unimplemented");
+        }
+    }
+}
+
+impl ToString for DataType {
+    fn to_string(&self) -> String {
+        match self {
+            DataType::Float32 => "f32".to_string(),
+            DataType::Float64 => "f64".to_string(),
+            DataType::Fixed(i, f) => format!("s{}.{}", i, f),
+        }
+    }
+}
+
+impl fmt::Display for ParseTypeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "unknown data type")
+    }
+}
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    #[structopt(short = "t", long = "type")]
-    datatype: Option<String>,
+    #[structopt(short = "t", long = "type", default_value = "f32")]
+    datatype: DataType,
+}
+
+fn convert(num: BigDecimal, typ: DataType) {
+    let s = match typ {
+        DataType::Float32 => num.to_f32().unwrap().to_string(),
+        DataType::Float64 => num.to_f64().unwrap().to_string(),
+        DataType::Fixed(_, _) => panic!("unimplemented"),
+    };
+    println!("{}", s);
 }
 
 fn main() -> io::Result<()> {
     let opt = Opt::from_args();
-    println!("{:#?}", opt);
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         let numstr = line.trim();
-        dbg!(numstr);
         let num = BigDecimal::from_decimal_str(numstr)
             .expect("could not parse number");
-        dbg!(num.to_f64().unwrap());
+        convert(num, opt.datatype);
     }
 
     Ok(())
