c4d8a373415d43befb8df7d57527709e5006f209
Adrian Sampson <adrian@radbox.org>
2021-08-02 15:05:50 -0700

    Split data type module

---
+++ src/datatype.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy)]
+pub enum DataType {
+    Float32,
+    Float64,
+    Fixed(bool, usize, usize),
+}
+
+#[derive(Debug, Clone)]
+pub struct ParseTypeError;
+
+impl FromStr for DataType {
+    type Err = ParseTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s == "f32" {
+            Ok(DataType::Float32)
+        } else if s == "f64" {
+            Ok(DataType::Float64)
+        } else {
+            panic!("unimplemented");
+        }
+    }
+}
+
+impl ToString for DataType {
+    fn to_string(&self) -> String {
+        match self {
+            DataType::Float32 => "f32".to_string(),
+            DataType::Float64 => "f64".to_string(),
+            DataType::Fixed(s, i, f) =>
+                format!("{}{}.{}", if *s { "s" } else { "u" }, i, f),
+        }
+    }
+}
+
+impl fmt::Display for ParseTypeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "unknown data type")
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,9 @@
+mod datatype;
 use structopt::StructOpt;
 use std::io::{self, BufRead};
-use std::str::FromStr;
 use std::string::ToString;
-use std::fmt;
 use fraction::{BigDecimal, ToPrimitive};
-
-#[derive(Debug, Clone, Copy)]
-enum DataType {
-    Float32,
-    Float64,
-    Fixed(bool, usize, usize),
-}
-
-#[derive(Debug, Clone)]
-struct ParseTypeError;
-
-impl FromStr for DataType {
-    type Err = ParseTypeError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s == "f32" {
-            Ok(DataType::Float32)
-        } else if s == "f64" {
-            Ok(DataType::Float64)
-        } else {
-            panic!("unimplemented");
-        }
-    }
-}
-
-impl ToString for DataType {
-    fn to_string(&self) -> String {
-        match self {
-            DataType::Float32 => "f32".to_string(),
-            DataType::Float64 => "f64".to_string(),
-            DataType::Fixed(s, i, f) =>
-                format!("{}{}.{}", if *s { "s" } else { "u" }, i, f),
-        }
-    }
-}
-
-impl fmt::Display for ParseTypeError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "unknown data type")
-    }
-}
+use datatype::DataType;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
