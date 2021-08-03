8630ad28a88066960c75ec5d71c199299ffb85da
Adrian Sampson <adrian@radbox.org>
2021-08-03 09:40:31 -0700

    Decode support for f64

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,35 @@ fn to_bytes(num: BigDecimal, typ: DataType) -> Box<[u8]> {
     }
 }
 
+fn hex_read<T: Read, const LEN: usize>(src: &mut T, buf: &mut [u8; LEN]) -> io::Result<bool> {
+    let mut enc_buf = vec![0u8; LEN * 2];
+    match src.read_exact(&mut enc_buf) {
+        Ok(()) => {}
+        Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(false),
+        Err(e) => return Err(e),
+    }
+    hex::decode_to_slice(enc_buf, buf).expect("could not parse hex data");
+    Ok(true)
+}
+
 fn read_from_hex<T: Read>(src: &mut T, typ: DataType) -> io::Result<Option<BigDecimal>> {
     match typ {
         DataType::Float32 => {
-            let mut enc_buf = [0u8; 8];
-            match src.read_exact(&mut enc_buf) {
-                Ok(()) => {}
-                Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
-                Err(e) => return Err(e),
-            }
             let mut buf = [0u8; 4];
-            hex::decode_to_slice(enc_buf, &mut buf).expect("could not parse hex data");
-            Ok(Some(f32::from_le_bytes(buf).into()))
+            if hex_read(src, &mut buf)? {
+                Ok(Some(f32::from_le_bytes(buf).into()))
+            } else {
+                Ok(None)
+            }
         }
-        DataType::Float64 => todo!(),
+        DataType::Float64 => {
+            let mut buf = [0u8; 8];
+            if hex_read(src, &mut buf)? {
+                Ok(Some(f64::from_le_bytes(buf).into()))
+            } else {
+                Ok(None)
+            }
+        }
         DataType::Fixed(_, _, _) => panic!("fixed point unimplemented"),
     }
 }
@@ -106,6 +121,15 @@ mod tests {
     #[test]
     fn text_to_hex_f32() {
         insta::assert_snapshot!(round_trip_hex(NUMBERS, DataType::Float32), @r###"
+        1.23
+        1.2345679
+        -5
+        "###);
+    }
+
+    #[test]
+    fn text_to_hex_f64() {
+        insta::assert_snapshot!(round_trip_hex(NUMBERS, DataType::Float64), @r###"
         1.23
         1.2345679
         -5
