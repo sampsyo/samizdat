497e961479a9ae79bf248e11201e5e635ce016b3
Adrian Sampson <adrian@radbox.org>
2021-08-05 17:09:02 -0700

    Drop fraction crate
    We now use num everywhere, which is a little simpler to
    understand/control. But I had to write my own decimal parser, which
    isn't too bad, and we lost arbitrary-precision decimal formatting, which
    is honestly probably fine.

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,20 @@ pub mod format;
 
 use datatype::DataType;
 use format::Format;
-use fraction::{BigDecimal, ToPrimitive};
+use num::rational::BigRational;
+use num::bigint::BigInt;
+use num::{ToPrimitive, FromPrimitive, Zero};
 use std::io::{self, BufRead, BufReader, Read, Write};
+use std::str::FromStr;
+use std::convert::TryInto;
 
-fn to_bytes(num: BigDecimal, typ: DataType) -> Box<[u8]> {
+fn to_bytes(num: BigRational, typ: DataType) -> Box<[u8]> {
     match typ {
         DataType::Float32 => Box::new(num.to_f32().unwrap().to_le_bytes()),
         DataType::Float64 => Box::new(num.to_f64().unwrap().to_le_bytes()),
-        DataType::Fixed(_, _, _) => panic!("fixed point unimplemented"),
+        DataType::Fixed(_, _, _) => {
+            panic!("fixed point unimplemented");
+        }
     }
 }
 
@@ -25,12 +31,12 @@ fn hex_read<T: Read, const LEN: usize>(src: &mut T, buf: &mut [u8; LEN]) -> io::
     Ok(true)
 }
 
-fn read_from_hex<T: Read>(src: &mut T, typ: DataType) -> io::Result<Option<BigDecimal>> {
+fn read_from_hex<T: Read>(src: &mut T, typ: DataType) -> io::Result<Option<BigRational>> {
     match typ {
         DataType::Float32 => {
             let mut buf = [0u8; 4];
             if hex_read(src, &mut buf)? {
-                Ok(Some(f32::from_le_bytes(buf).into()))
+                Ok(BigRational::from_f32(f32::from_le_bytes(buf)))
             } else {
                 Ok(None)
             }
@@ -38,7 +44,7 @@ fn read_from_hex<T: Read>(src: &mut T, typ: DataType) -> io::Result<Option<BigDe
         DataType::Float64 => {
             let mut buf = [0u8; 8];
             if hex_read(src, &mut buf)? {
-                Ok(Some(f64::from_le_bytes(buf).into()))
+                Ok(BigRational::from_f64(f64::from_le_bytes(buf)))
             } else {
                 Ok(None)
             }
@@ -47,10 +53,38 @@ fn read_from_hex<T: Read>(src: &mut T, typ: DataType) -> io::Result<Option<BigDe
     }
 }
 
-fn read_from_text<T: BufRead>(src: &mut T) -> io::Result<Option<BigDecimal>> {
+/// Parse decimal numbers of the format [-]I[.F], where I and F are strings of decimals.
+/// TODO: Should return a Result.
+fn parse_decimal(s: &str) -> BigRational {
+    let mut split = s.splitn(2, '.');
+
+    let int: BigInt = FromStr::from_str(
+        split.next().expect("missing integer part")
+    ).expect("could not parse integer part");
+
+    let frac = match split.next() {
+        Some(frac_s) => {
+            let mant = BigInt::from_str(frac_s).expect("could not parse fractional part");
+            let exp = frac_s.len();
+            let mag = BigInt::from(10u8).pow(exp.try_into().unwrap());  // 10^exp
+            BigRational::new(mant, mag)  // mant * 10^-exp
+        },
+        None => BigRational::zero()
+    };
+
+    BigRational::from_integer(int) + frac
+}
+
+/// Format a number for text output. Currently not arbitrary-precision; we just format the f64
+/// approximation.
+fn fmt_decimal(r: &BigRational) -> String {
+    format!("{}", r.to_f64().unwrap())
+}
+
+fn read_from_text<T: BufRead>(src: &mut T) -> io::Result<Option<BigRational>> {
     let mut buf = String::new();
     Ok(if src.read_line(&mut buf)? != 0 {
-        Some(BigDecimal::from_decimal_str(buf.trim()).expect("could not parse number"))
+        Some(parse_decimal(buf.trim()))
     } else {
         None
     })
@@ -80,7 +114,7 @@ pub fn convert<I: BufRead, O: Write>(
                         write!(output, "{}", hex::encode(bytes))?;
                     }
                     Format::Text => {
-                        writeln!(output, "{}", num)?;
+                        writeln!(output, "{}", fmt_decimal(&num))?;
                     }
                 }
             }
@@ -121,8 +155,8 @@ mod tests {
     #[test]
     fn text_to_hex_f32() {
         insta::assert_snapshot!(round_trip_hex(NUMBERS, DataType::Float32), @r###"
-        1.23
-        1.2345679
+        1.2300000190734863
+        1.2345678806304932
         -5
         "###);
     }
@@ -131,7 +165,7 @@ mod tests {
     fn text_to_hex_f64() {
         insta::assert_snapshot!(round_trip_hex(NUMBERS, DataType::Float64), @r###"
         1.23
-        1.234567890123457
+        1.2345678901234567
         -5
         "###);
     }
